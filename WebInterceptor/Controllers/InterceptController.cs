06a4a3e281a6b9fe5776b09890d9b4fc5f3001ec
Theo Danzfuss <theodanzfuss@gmail.com>
2018-08-14 14:09:13 +0200

    Initial commit

---
+++ WebInterceptor/Controllers/InterceptController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Swashbuckle.Swagger.Annotations;
+using WebInterceptor.Models;
+
+namespace WebInterceptor.Controllers
+{
+    
+    public class InterceptController : ApiController
+    {
+        // POST api/values
+        [SwaggerOperation("Create")]
+        [SwaggerResponse(HttpStatusCode.Created)]
+        public HttpResponseMessage Post([FromBody] InterceptorContext value)
+        {
+            if (Request.Headers.Contains("Authorization"))
+            {
+                return Request.CreateResponse<String>(HttpStatusCode.OK, String.Join(",", Request.Headers.GetValues("Authorization")));
+                // if the request header contains the auth token then strip it out.
+            }
+            return Request.CreateResponse<String>(HttpStatusCode.OK, "NO AUTHORIZATION HEADER");
+        }
+    }
+}
---
+++ WebInterceptor/Models/InterceptorContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebInterceptor.Models
+{
+    public class InterceptorContext
+    {
+        public bool staff;
+        public string authenticationToken;
+        public string dataType;
+        public string audience;
+        public string segment;
+        public string partyId;
+        public object dataModel;
+    }
+}
